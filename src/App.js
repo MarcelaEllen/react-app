de1be0ed519fd41b1f742a20f52fa3c8d044b456
Marcela Ellen <marcela.ellen867@gmail.com>
2023-10-16 22:11:56 -0300

    meu nome na tela

--- src/App.js
+++ src/App.js
@@ -3,26 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Formulário Simples</h1>
-      <form>
-        <div>
-          <label htmlFor="nome">Nome:</label>
-          <input
-            type="text"
-            id="nome"
-            placeholder="Nome"
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Email"
-          />
-        </div>
-        <button type="submit">Enviar</button>
-      </form>
+      <h1>MARCELA ELLEN</h1>
     </div>
   );
 }
